bfd94b8d569a8a7a528f9c4a614971f2453ec879
unknown <hughesgeorgeking@gmail.com>
2023-09-17 09:59:57 +0000

    changes

--- app/page.jsx
+++ app/page.jsx
@@ -3,8 +3,8 @@ import PostsList from "@/components/PostsList";
 export default function Home() {
   return (
     <div>
-      {/* <PostsList /> */}
-      <h1>Hello World</h1>
+      <PostsList />
+      {/* <h1>Hello World</h1> */}
     </div>
   );
 }
