4edee98fc2d5bb3f4d1d425b5692d2f39bbc0604
unknown <hughesgeorgeking@gmail.com>
2023-09-17 00:56:42 +0000

    changes

--- app/edit-post/[id]/page.jsx
+++ app/edit-post/[id]/page.jsx
@@ -1,10 +1,28 @@
 import EditPostForm from "@/components/EditPostForm";
-import React from "react";
 
-const page = () => {
+const getPostById = async (id) => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/post/${id}`, {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch post");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const page = async ({ params }) => {
+  const { id } = params;
+  const posts = await getPostById(id);
+
   return (
     <div>
-      <EditPostForm />
+      <EditPostForm posts={posts} />
     </div>
   );
 };
--- components/EditPostForm.jsx
+++ components/EditPostForm.jsx
@@ -1,21 +1,59 @@
-import React from "react";
+"use client";
 
-const EditPostForm = () => {
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+const EditPostForm = ({ id, title, description }) => {
+  const router = useRouter();
+
+  const [newTitle, setNewTitle] = useState(title);
+  const [newDescription, setNewDescription] = useState(description);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch(`http://localhost:3000/api/post/${id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ newTitle, newDescription }),
+      });
+
+      if (!res.ok) {
+        throw new Error("Failed to fatch post");
+      }
+
+      router.push("/");
+      // return res.json();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <form className="flex flex-col gap-3">
+    <form onSubmit={handleSubmit} className="flex flex-col gap-3">
       <input
+        onChange={(e) => setNewTitle(e.target.value)}
+        value={newTitle}
         type="text"
         placeholder="Post Title"
         className="border border-slate-500 px-8 py-3"
       />
 
       <input
+        onChange={(e) => setNewDescription(e.target.value)}
+        value={newDescription}
         type="text"
         placeholder="Post Description"
         className="border border-slate-500 px-8 py-3"
       />
 
-      <button className="bg-green-600 text-white font-bold px-6 py-3 w-fit">
+      <button
+        type="submit"
+        className="bg-green-600 text-white font-bold px-6 py-3 w-fit"
+      >
         Update Post
       </button>
     </form>
